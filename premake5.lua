0031ba91811022b85d07d1c13c46aac9d227d4bb
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-17 22:56:59 +0100

    fix: Disable warnings

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
     staticruntime "off"
+	disablewarnings { "4996", "4005" }
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
