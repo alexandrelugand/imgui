c489e781f948683c73abeca6b4f810bc13a6627e
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-17 23:52:04 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,49 +1,52 @@
-project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "off"
-	disablewarnings { "4996", "4005" }
-    architecture "x64"
-	targetdir ("%{wks.location}/bin/" .. outputdir)
-	objdir ("%{wks.location}/obj/" .. tmpdir)
+local _files =
+{
+	"imconfig.h",
+	"imgui.h",
+	"imgui.cpp",
+	"imgui_draw.cpp",
+	"imgui_internal.h",
+	"imgui_tables.cpp",
+	"imgui_widgets.cpp",
+	"imstb_rectpack.h",
+	"imstb_textedit.h",
+	"imstb_truetype.h",
+	"imgui_demo.cpp",
+	"backends/imgui_impl_glfw.cpp",
+	"backends/imgui_impl_glfw.cpp",
+	"backends/imgui_impl_opengl3.h",
+	"backends/imgui_impl_opengl3.cpp"
+}
 
-	files
-	{
-		"imconfig.h",
-    	"imgui.h",
-    	"imgui.cpp",
-    	"imgui_draw.cpp",
-    	"imgui_internal.h",
-    	"imgui_tables.cpp",
-    	"imgui_widgets.cpp",
-    	"imstb_rectpack.h",
-    	"imstb_textedit.h",
-    	"imstb_truetype.h",
-    	"imgui_demo.cpp",
-    	"backends/imgui_impl_glfw.cpp",
-    	"backends/imgui_impl_glfw.cpp",
-    	"backends/imgui_impl_opengl3.h",
-    	"backends/imgui_impl_opengl3.cpp"
-	}
+local _includedirs =
+{
+	".",
+	"./backends",
+	"../GLFW/include"
+}
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
+group "Dependencies"
+	project "ImGui"
+		kind "StaticLib"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+		targetdir ("%{wks.location}/bin/" .. outputdir)
+		objdir ("%{wks.location}/bin-int/" .. tmpdir)
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
+		files(_files)
+		includedirs(_includedirs)
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+		filter "system:windows"
+			systemversion "latest"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
 
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
+
+group ""
