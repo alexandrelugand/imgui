03d3c0ea2ce79ef165a7f9c47bf9412f27e3e5a8
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-17 23:34:16 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "ImGui"
     staticruntime "off"
 	disablewarnings { "4996", "4005" }
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir)
+	objdir ("%{wks.location}/obj/" .. tmpdir)
 
 	files
 	{
