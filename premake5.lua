67ca613db7392950552032aa554db2abfd57feb6
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-21 17:40:25 +0100

    Restore premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,53 +1,44 @@
-local _files =
-{
-	"imconfig.h",
-	"imgui.h",
-	"imgui.cpp",
-	"imgui_draw.cpp",
-	"imgui_internal.h",
-	"imgui_tables.cpp",
-	"imgui_widgets.cpp",
-	"imstb_rectpack.h",
-	"imstb_textedit.h",
-	"imstb_truetype.h",
-	"imgui_demo.cpp",
-	"backends/imgui_impl_glfw.cpp",
-	"backends/imgui_impl_glfw.cpp",
-	"backends/imgui_impl_opengl3.h",
-	"backends/imgui_impl_opengl3.cpp"
-}
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
 
-local _includedirs =
-{
-	".",
-	"./backends",
-	"../GLFW/include"
-}
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-group "Dependencies"
-	project "ImGui"
-		kind "StaticLib"
-		language "C++"
-		cppdialect "C++20"
-		staticruntime "off"
-		architecture "x64"
-        disablewarnings { "4996", "4005" }
-		targetdir ("%{wks.location}/bin/" .. outputdir)
-		objdir ("%{wks.location}/obj/" .. tmpdir)
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
 
-		files(_files)
-		includedirs(_includedirs)
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
 
-		filter "system:windows"
-			systemversion "latest"
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
 
-		filter "configurations:Debug"
-			runtime "Debug"
-			symbols "on"
-			optimize "Off"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-		filter "configurations:Release"
-			runtime "Release"
-			optimize "full"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
-group ""
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
