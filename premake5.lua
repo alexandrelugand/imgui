4eef4c9b4d0537d20cb9f051239402a9f827da3a
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-21 17:41:48 +0100

    Include Vulkan backend

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,56 @@
-project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "off"
+local _files =
+{
+	"imconfig.h",
+	"imgui.h",
+	"imgui.cpp",
+	"imgui_draw.cpp",
+	"imgui_internal.h",
+	"imgui_tables.cpp",
+	"imgui_widgets.cpp",
+	"imstb_rectpack.h",
+	"imstb_textedit.h",
+	"imstb_truetype.h",
+	"imgui_demo.cpp",
+	"backends/imgui_impl_glfw.cpp",
+	"backends/imgui_impl_glfw.cpp",
+	"backends/imgui_impl_opengl3.h",
+	"backends/imgui_impl_opengl3.cpp",
+    "backends/imgui_impl_vulkan.h",
+	"backends/imgui_impl_vulkan.cpp"
+}
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+local _includedirs =
+{
+	".",
+	"./backends",
+	"../GLFW/include",
+	"%{includeDir.VulkanSDK}",
+}
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+group "Dependencies"
+	project "ImGui"
+		kind "StaticLib"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+        disablewarnings { "4996", "4005" }
+		targetdir ("%{wks.location}/bin/" .. outputdir)
+		objdir ("%{wks.location}/obj/" .. tmpdir)
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
+		files(_files)
+		includedirs(_includedirs)
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
+		filter "system:windows"
+			systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
 
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+group ""
