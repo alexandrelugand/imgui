027bd264f943b3ac68abc9eaf81f7146b1bf2ba6
Alexandre Lugand <alexandre.lugand@gmail.com>
2025-12-01 00:33:48 +0100

    imp: refactoring Vulkan code in many classes (instance, device, swapchain) imp: remove VulkanInit. Use NRVHI shared library) and dispatch loader dynamic imp: use nameof to display API string

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,9 @@ local _files =
 	"backends/imgui_impl_opengl3.h",
 	"backends/imgui_impl_opengl3.cpp",
     "backends/imgui_impl_vulkan.h",
-	"backends/imgui_impl_vulkan.cpp"
+	"backends/imgui_impl_vulkan.cpp",
+    "backends/imgui_impl_dx12.h",
+    "backends/imgui_impl_dx12.cpp"
 }
 
 local _includedirs =
